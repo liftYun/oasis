4dea0c445c31a3d2f6684c421b34f32cde9237b1
이도윤 <liftyun@gmail.com>
2025-09-08 12:00:15 +0900

    [BE] fix: add gitignore_buildFolrder

--- back/oasis/src/test/java/org/muhan/oasis/OasisApplicationTests.java
+++ back/oasis/src/test/java/org/muhan/oasis/OasisApplicationTests.java
@@ -1,9 +1,11 @@
 package org.muhan.oasis;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Transactional
 class OasisApplicationTests {
 
     @Test
