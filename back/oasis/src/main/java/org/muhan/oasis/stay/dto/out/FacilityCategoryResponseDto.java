27bb2279e62f2d6b6506b90cc52883662e825f1f
yoonsu0325 <yoonsu0325@gmail.com>
2025-09-09 21:11:01 +0900

    [BE] fix: 편의시설 추가 수정

--- back/oasis/src/main/java/org/muhan/oasis/stay/dto/out/FacilityCategoryResponseDto.java
+++ back/oasis/src/main/java/org/muhan/oasis/stay/dto/out/FacilityCategoryResponseDto.java
@@ -3,6 +3,7 @@ package org.muhan.oasis.stay.dto.out;
 import org.muhan.oasis.stay.entity.FacilityEntity;
 import org.muhan.oasis.stay.entity.RegionEntity;
 import org.muhan.oasis.stay.entity.StayFacilityEntity;
+import org.muhan.oasis.valueobject.Category;
 
 import java.util.*;
 import java.util.function.Function;
@@ -18,7 +19,7 @@ public record FacilityCategoryResponseDto(
             return List.of();
         }
 
-        Map<FacilityEntity, List<FacilityEntity>> grouped = facilities.stream()
+        Map<Category, List<FacilityEntity>> grouped = facilities.stream()
                 .map(StayFacilityEntity::getFacility)
                 .filter(Objects::nonNull)
                 .collect(Collectors.groupingBy(
@@ -35,13 +36,11 @@ public record FacilityCategoryResponseDto(
                         )
                 ));
 
-        // 2) 카테고리 순서 정렬(ENUM 순서 기준) + 내부 시설 리스트를 DTO로 변환하며 id 기준 정렬
         return grouped.entrySet().stream()
                 .map(e -> new FacilityCategoryResponseDto(
-                        e.getKey().getName(),
+                        e.getKey().name(),
                         e.getValue().stream()
                                 .map(FacilityResponseDto::from)
-                                // id가 int가 아닌 Long이면 comparingLong(FacilityResponseDto::id)로 변경
                                 .sorted(Comparator.comparingLong(FacilityResponseDto::id))
                                 .toList()
                 ))
