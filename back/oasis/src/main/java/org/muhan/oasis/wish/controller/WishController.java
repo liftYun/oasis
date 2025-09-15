162d2c4c85d0eeb5ede05a092ce3fdfb2c3e1566
DESKTOP-DLOPF19\SSAFY <yoonsu0325@gmail.com>
2025-09-15 11:48:17 +0900

    [BE] feat: swagger annotation 추가

--- back/oasis/src/main/java/org/muhan/oasis/wish/controller/WishController.java
+++ back/oasis/src/main/java/org/muhan/oasis/wish/controller/WishController.java
@@ -1,5 +1,11 @@
 package org.muhan.oasis.wish.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.muhan.oasis.common.base.BaseResponse;
 import org.muhan.oasis.security.dto.out.CustomUserDetails;
@@ -24,6 +30,35 @@ public class WishController {
 
     private final WishService wishService;
 
+    @Operation(
+            summary = "관심 숙소 생성",
+            description = "사용자의 관심 숙소를 생성합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "생성됨",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    name = "성공 응답",
+                                    value = """
+                                            {
+                                                "httpStatus": "CREATED",
+                                                "isSuccess": true,
+                                                "message": "리소스가 생성되었습니다.",
+                                                "code": 201,
+                                                "result": null
+                                            }
+                                            """
+                            )
+                    )
+            ),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content),
+            @ApiResponse(responseCode = "401", description = "인증 필요", content = @Content),
+            @ApiResponse(responseCode = "409", description = "이미 찜된 경우 등 충돌", content = @Content),
+            @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content)
+    })
     @PostMapping
     public ResponseEntity<BaseResponse<?>> addWish(
             @AuthenticationPrincipal CustomUserDetails userDetails,
