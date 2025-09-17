ba71bed20768f318029ddf4ea5f65d44f3268242
yuju9 <lah0930@gmail.com>
2025-09-17 09:27:34 +0900

    fix: approve/lock 금액 단위 처리 수정 및 불일치 해결

--- back/oasis/src/main/java/org/muhan/oasis/reservation/dto/in/ApproveRequestDto.java
+++ back/oasis/src/main/java/org/muhan/oasis/reservation/dto/in/ApproveRequestDto.java
@@ -4,18 +4,20 @@ import lombok.Builder;
 import lombok.Data;
 import org.muhan.oasis.reservation.vo.in.RegistReservationRequestVo;
 
+import java.math.BigDecimal;
+
 @Data
 @Builder
 public class ApproveRequestDto {
     private String userUUID;
-    private String amountUSDC;  // "100.00" (소수점 2자리까지)
-    private String feeUSDC;
+    private BigDecimal amountUSDC;  // "100.00" (소수점 2자리까지)
+    private BigDecimal feeUSDC;
 
-    public static ApproveRequestDto from(RegistReservationRequestVo dto, String userUUID) {
+    public static ApproveRequestDto from(RegistReservationRequestVo vo, String userUUID) {
         return ApproveRequestDto.builder()
                 .userUUID(userUUID)
-                .amountUSDC(String.valueOf(dto.getPayment()))
-                .feeUSDC("0") //수수료
+                .amountUSDC(BigDecimal.valueOf(vo.getPayment()))
+                .feeUSDC(BigDecimal.ZERO) //수수료
                 .build();
     }
 }
