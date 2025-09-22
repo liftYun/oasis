3cf75b7e8ae71544d725fc5847640aa880557e6b
DESKTOP-DLOPF19\SSAFY <yoonsu0325@gmail.com>
2025-09-22 14:20:01 +0900

    [BE] feat: 리뷰 작성 시 리뷰 요약 메세지 전송

--- back/oasis/src/main/java/org/muhan/oasis/openAI/dto/in/ReviewRequestDto.java
+++ back/oasis/src/main/java/org/muhan/oasis/openAI/dto/in/ReviewRequestDto.java
@@ -2,6 +2,7 @@ package org.muhan.oasis.openAI.dto.in;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.muhan.oasis.review.entity.ReviewEntity;
 import org.muhan.oasis.valueobject.Language;
 
 @Getter
@@ -9,4 +10,16 @@ import org.muhan.oasis.valueobject.Language;
 public class ReviewRequestDto {
     String content;
     Language language;
+
+    public static ReviewRequestDto from(ReviewEntity r, Language language){
+        String content;
+        if(language.equals(Language.KOR)){
+            content = r.getContent();
+        }
+        else content = r.getContentEng();
+        return new ReviewRequestDto(
+                content,
+                language
+        );
+    }
 }
