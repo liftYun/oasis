6b3834e36ff266e39187e15440015b3aee4950b5
yuju9 <lah0930@gmail.com>
2025-09-23 16:42:34 +0900

    fix: infura연결을 http 에서 웹소켓 기반으로 전환

--- back/oasis/src/main/java/org/muhan/oasis/config/Web3jConfig.java
+++ back/oasis/src/main/java/org/muhan/oasis/config/Web3jConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.protocol.websocket.WebSocketService;
+
+import java.net.ConnectException;
 
 @Configuration
 public class Web3jConfig {
@@ -13,7 +16,9 @@ public class Web3jConfig {
     private String infuraUrl;
 
     @Bean
-    public Web3j web3j() {
-        return Web3j.build(new HttpService(infuraUrl));
+    public Web3j web3j() throws ConnectException {
+        WebSocketService webSocketService = new WebSocketService(infuraUrl, true);
+        webSocketService.connect();
+        return Web3j.build(webSocketService);
     }
 }
