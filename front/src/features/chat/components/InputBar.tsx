3304de621d0c7180b2b4fe2a5c9f707e58577738
김수민 <gkgkgkgk22@naver.com>
2025-09-25 14:57:24 +0900

    [FE] fix: 채팅방 확대 현상 수정

--- front/src/features/chat/components/InputBar.tsx
+++ front/src/features/chat/components/InputBar.tsx
@@ -87,7 +87,7 @@ export default function InputBar({ onSend }: InputBarProps) {
           value={text}
           onChange={handleTextChange}
           onKeyDown={handleKeyDown}
-          className="flex-1 min-w-0 bg-transparent outline-none text-sm px-1"
+          className="flex-1 min-w-0 bg-transparent outline-none text-sm px-1 text-base"
           placeholder={t.inputPlaceholder}
           maxLength={500}
         />
